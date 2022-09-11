a89aa7ed1f9b8fe09a21450aa265c17ddb61a805
Andres Guerrero <andresmgsl@gmail.com>
2022-09-11 18:14:57 +0100

    fix: modal

--- apps/web-client/src/app/shared/components/modal.component.ts
+++ apps/web-client/src/app/shared/components/modal.component.ts
@@ -26,16 +26,16 @@ import { Component, EventEmitter, HostBinding, Output } from '@angular/core';
 
     <!-- outer borders -->
     <div
-      class="bp-skin-outer-metal-border-right absolute -right-5 h-5/6 top-0 bottom-0 my-auto mx-0 z-50"
+      class="bp-skin-outer-metal-border-right absolute -right-5 h-5/6 top-0 bottom-0 my-auto mx-0 z-20 rounded-2xl"
     ></div>
     <div
-      class="bp-skin-outer-metal-border-left absolute -left-5 h-5/6 top-0 bottom-0 my-auto mx-0 z-50"
+      class="bp-skin-outer-metal-border-left absolute -left-5 h-5/6 top-0 bottom-0 my-auto mx-0 z-20 rounded-2xl"
     ></div>
     <div
-      class="bp-skin-outer-metal-border-bottom absolute -bottom-4 w-5/6 left-0 right-0 mx-auto my-0 z-50"
+      class="bp-skin-outer-metal-border-bottom absolute -bottom-4 w-5/6 left-0 right-0 mx-auto my-0 z-20 rounded-2xl"
     ></div>
     <div
-      class="bp-skin-outer-metal-border-top absolute -top-4 w-5/6 left-0 right-0 mx-auto my-0 z-20"
+      class="bp-skin-outer-metal-border-top absolute -top-4 w-5/6 left-0 right-0 mx-auto my-0 z-20 rounded-2xl"
     ></div>
 
     <!-- modal content -->
