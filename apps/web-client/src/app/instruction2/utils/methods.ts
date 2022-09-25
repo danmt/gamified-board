b38638ddbd282152f76cc8e20fd9d99832b7a83f
Daniel Marin <danielarturomt@gmail.com>
2022-09-25 18:56:50 +0100

    feat: add different labels per instruction node kind

--- apps/web-client/src/app/instruction2/utils/methods.ts
+++ apps/web-client/src/app/instruction2/utils/methods.ts
@@ -2,18 +2,59 @@ import { NodeSingular } from 'cytoscape';
 import { InstructionNode } from './types';
 
 export const instructionNodeLabelFunction = (node: InstructionNode) => {
-  return `
-    <div class="w-[280px] h-[85px] flex gap-2 items-center px-8 bg-[length:280px_85px] bg-[url('assets/images/node.png')] z-50">
-      <div 
-          class="w-[56px] h-[52px] shrink-0 rounded-lg border-gray-700 border-2 bg-cover bg-center"
-          style="background-image: url(${node.data.thumbnailUrl});">
-      </div>
-      <div style="font-family: 'Courier New', Courier, monospace">
-        <h2 class="text-xl mt-2 text-white">${node.data.name}</h2>
-        <p class="italic text-gray-400">${node.kind}</p>
-      </div>
-    </div>
-  `;
+  switch (node.kind) {
+    case 'signer': {
+      return `
+        <div class="w-[280px] h-[85px] flex gap-2 items-center px-8 bg-[length:280px_85px] bg-[url('assets/images/node.png')] z-50">
+          <div 
+            class="w-[56px] h-[52px] shrink-0 rounded-lg border-gray-700 border-2 bg-cover bg-center"
+            style="background-image: url(${node.data.thumbnailUrl});">
+          </div>
+          <div style="font-family: 'Courier New', Courier, monospace">
+            <h2 class="text-xl mt-2 text-white">${node.data.name}</h2>
+            <p class="text-gray-400">
+              ${node.kind} 
+              
+              ${
+                node.data.isMutable &&
+                '<span class="text-xs italic">mutable</span>'
+              }
+            </p>
+          </div>
+        </div>
+      `;
+    }
+
+    case 'document': {
+      return `
+        <div class="w-[280px] h-[85px] flex gap-2 items-center px-8 bg-[length:280px_85px] bg-[url('assets/images/node.png')] z-50">
+          <div 
+              class="w-[56px] h-[52px] shrink-0 rounded-lg border-gray-700 border-2 bg-cover bg-center"
+              style="background-image: url(${node.data.thumbnailUrl});">
+          </div>
+          <div style="font-family: 'Courier New', Courier, monospace">
+            <h2 class="text-xl mt-2 text-white">${node.data.name}</h2>
+            <p class="italic text-gray-400">${node.kind}</p>
+          </div>
+        </div>
+      `;
+    }
+
+    case 'application': {
+      return `
+        <div class="w-[280px] h-[85px] flex gap-2 items-center px-8 bg-[length:280px_85px] bg-[url('assets/images/node.png')] z-50">
+          <div 
+              class="w-[56px] h-[52px] shrink-0 rounded-lg border-gray-700 border-2 bg-cover bg-center"
+              style="background-image: url(${node.data.thumbnailUrl});">
+          </div>
+          <div style="font-family: 'Courier New', Courier, monospace">
+            <h2 class="text-xl mt-2 text-white">${node.data.name}</h2>
+            <p class="italic text-gray-400">${node.data.ref.name}</p>
+          </div>
+        </div>
+      `;
+    }
+  }
 };
 
 export const instructionCanConnectFunction = (
